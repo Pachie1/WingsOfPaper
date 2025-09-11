52a545abf1441e835aaea4c4e157e92370158141
Gaspar Orma <gasparorma13@gmail.com>
2025-09-11 01:42:14 -0300

    Initial commit
    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletLife = 1f;
+    public float rotation = 0f;
+    public float speed = 1f;
+
+    private Vector2 spawnPoint;
+    private float timer = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawnPoint = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        transform.position = Movement(timer);
+    }
+
+    private Vector2 Movement(float timer)
+    {
+        float x = timer * speed * transform.up.x;
+        float y = timer * speed * transform.up.y;
+
+        return new Vector2 (x+spawnPoint.x, y+spawnPoint.y);
+    }
+}
---
+++ Assets/Scripts/PlayerShootProjectiles.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerShootProjectiles : MonoBehaviour
+{
+    
+    
+    [Header("Player component references")]
+    [SerializeField] Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
