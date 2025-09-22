cdcab6f16e95e1862d72f1b8fb24fa88b3fdc52a
Gaspar Orma <gasparorma13@gmail.com>
2025-09-22 00:06:02 -0300

    TP V0.1 Corrections
    1. Fix enemy dead animation
    2. Added tilemaps
    3. Removed unused textures
    4. Created TilePallets: TP_Ocean - TP_Ground - TP Buildings
    5. Folder structure

---
+++ Assets/Code/Scripts/EnemyCollision.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class EnemyCollis : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField] public float HitPoints = 4f;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Projectile"))
+        {
+            Destroy(other.gameObject);
+            Bullet bullet = other.GetComponent<Bullet>();
+            if (bullet) 
+            {
+                takeDamage(bullet.damage);
+            }
+            
+        }
+    }
+
+    private void takeDamage(float x)
+    {
+        animator.SetTrigger("OnHit");
+        HitPoints -= x;
+        if (HitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("Dead");
+        Destroy(GetComponent<BoxCollider2D>());
+    }
+    private void OnDeadAnimation() 
+    {
+        Destroy(gameObject,0f);
+    }
+}
---
+++ Assets/Code/Scripts/StartGame.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject canvas;
+
+    [SerializeField] private InputActionReference enterAction;
+
+    private void Awake()
+    {
+        Time.timeScale = 0f;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (anyKey.triggered)
+        if (enterAction.action.IsPressed())
+        {
+            Time.timeScale = 1f;
+            canvas.SetActive(false);
+        }
+    }
+}
